8058b51f56780038a31fd6668f6b59026359238b
Nam Pham <puma.puma07@gmail.com>
2014-05-30 11:20:19 +0700

    Fix attributes for device to support Hash

--- lib/push/message_apns.rb
+++ lib/push/message_apns.rb
@@ -71,7 +71,7 @@ module Push
       json['aps']['badge'] = badge if badge
       json['aps']['sound'] = sound if sound
       json['aps']['content-available'] = content_available if content_available
-      attributes_for_device.each { |k, v| json[k.to_s] = v.to_s } if attributes_for_device
+      attributes_for_device.each { |k, v| json[k.to_s] = v } if attributes_for_device
       json
     end
 
